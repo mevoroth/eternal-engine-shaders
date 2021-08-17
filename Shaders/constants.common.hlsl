c50029cad775686230435a8f99578fd607b66b8d
Daniel Quach <daniel@quach.info>
2021-08-17 20:59:51 +0200

    Constants

--- Shaders/constants.common.hlsl
+++ Shaders/constants.common.hlsl
@@ -1,6 +1,11 @@
 #ifndef _CONSTANTS_COMMON_HLSL_
 #define _CONSTANTS_COMMON_HLSL_
 
-#define EPSILON	(1e-5)
+#define EPSILON						(1e-5f)
+#define PI							(3.14159265358979323846f)
+#define PI_2						(PI * 2.0f)
+#define PI_RCP						(1.0f / PI)
+#define PI_2_RCP					(1.0f / PI_2)
+#define MIN_DIELECTRICS_F0			(0.04f)
 
 #endif
