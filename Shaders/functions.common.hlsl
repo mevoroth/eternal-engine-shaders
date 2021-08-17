bc0f291206669a70cf6a6b6122a7f2bbc6cb7bf9
Daniel Quach <daniel@quach.info>
2021-08-17 21:00:00 +0200

    Common functions

--- Shaders/functions.common.hlsl
+++ Shaders/functions.common.hlsl
@@ -1,9 +1,21 @@
 #ifndef _FUNCTIONS_COMMON_HLSL_
 #define _FUNCTIONS_COMMON_HLSL_
 
+float Luminance(float3 Color)
+{
+	return dot(Color, float3(0.2126f, 0.7152f, 0.0722f));
+}
+
 float2 UVToClipXY(float2 UV)
 {
-    return UV * float2(2.0f, -2.0f) + float2(-1.0f, 1.0f);
+	return UV * float2(2.0f, -2.0f) + float2(-1.0f, 1.0f);
+}
+
+float3 UVDepthToWorldPosition(float2 UV, float Depth, float4x4 ClipToWorld)
+{
+	float4 ClipPosition		= float4(UVToClipXY(UV), Depth, 1.0f);
+	float4 WorldPosition	= mul(ClipToWorld, ClipPosition);
+	return WorldPosition.xyz / WorldPosition.w;
 }
 
 #endif
