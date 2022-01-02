b119e303443efb18426b08f6cbc5d494a8f16331
Daniel Quach <daniel@quach.info>
2022-01-02 16:14:45 +0100

    Object mesh shader

---
+++ object.ms.hlsl
@@ -0,0 +1,9 @@
+#include "object.common.hlsl"
+#include "ShadersReflection/HLSLPerDrawConstants.hpp"
+
+[numthreads(64, 1, 1)]
+[outputtopology("triangle")]
+void MS( uint3 DTid : SV_DispatchThreadID )
+{
+	
+}
