0c49ab9863673f8cdfc5ff53a3ca7d36260dd073
Daniel Quach <daniel@quach.info>
2022-11-06 16:09:18 +0100

    Constant buffer cross platform

--- Shaders/dx12.common.hlsl
+++ Shaders/dx12.common.hlsl
@@ -7,4 +7,6 @@
 #define REGISTER_U(ShaderResource, Index, Set)							DX12_CONCATENATE_REGISTER(ShaderResource, : register(u##Index))
 #define REGISTER_S(ShaderResource, Index, Set)							DX12_CONCATENATE_REGISTER(ShaderResource, : register(s##Index))
 
+#define CONSTANT_BUFFER(StructName, BufferName, Index, Set)				REGISTER_B(ConstantBuffer<StructName##_Internal> BufferName##_Internal, Index, Set)
+
 #endif
--- Shaders/vulkan.common.hlsl
+++ Shaders/vulkan.common.hlsl
@@ -7,4 +7,10 @@
 #define REGISTER_U(ShaderResource, Index, Set)							VULKAN_CONCATENATE_REGISTER([[vk::binding(Index /*+ REGISTER_OFFSET_UNORDERED_ACCESSES*/, Set)]],	ShaderResource)
 #define REGISTER_S(ShaderResource, Index, Set)							VULKAN_CONCATENATE_REGISTER([[vk::binding(Index /*+ REGISTER_OFFSET_SAMPLERS*/, Set)]],				ShaderResource)
 
+#define CONSTANT_BUFFER(StructName, BufferName, Index, Set)				struct StructName##_Internal { \
+																			StructName InternalValue; \
+																		}; \
+																		REGISTER_B(ConstantBuffer<StructName##_Internal> BufferName##_Internal, Index, Set); \
+																		static StructName BufferName = BufferName##_Internal.InternalValue
+
 #endif
