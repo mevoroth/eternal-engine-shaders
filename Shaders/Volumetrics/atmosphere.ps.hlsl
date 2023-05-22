7a48722f589eb16fdc05014c5bf926ecd028fabf
Daniel Quach <daniel@quach.info>
2023-05-23 01:08:24 +0200

    Filled data for atmosphere

--- Shaders/Volumetrics/atmosphere.ps.hlsl
+++ Shaders/Volumetrics/atmosphere.ps.hlsl
@@ -11,39 +11,54 @@ CONSTANT_BUFFER(PerViewCubeMapConstants, PerViewCubeMapConstantBuffer,	1, 0);
 REGISTER_B_PER_VIEW_CONSTANT_BUFFER(									1, 0);
 #endif
 
+static const float3 EarthRayleighScattering = float3(5.8e-6f, 13.6e-6f, 33.1e-6f) * KM_TO_M;
+static const float3 EarthMieScattering		= (float3)(2.0e-6f  * KM_TO_M);
+
 struct PSOut
 {
 	float4 OutLuminance : SV_Target0;
 };
 
+float3 GetRayOrigin( PSIn IN )
+{
+#if USE_MULTIPLE_LAYER_RENDER_TARGETS == MULTIPLE_LAYER_RENDER_TARGETS_CUBEMAP
+	uint CubeMapFace				= IN.RenderTargetArrayIndex;
+	PerViewConstants CurrentView	= PerViewCubeMapConstantBuffer.PerViewFace[CubeMapFace];
+#else // USE_MULTIPLE_LAYER_RENDER_TARGETS == MULTIPLE_LAYER_RENDER_TARGETS_NONE
+	PerViewConstants CurrentView	= PerViewConstantBuffer;
+#endif
+	return CurrentView.ViewPosition.xyz;
+}
+
 float3 GetRayDirection( PSIn IN )
 {
 #if USE_MULTIPLE_LAYER_RENDER_TARGETS == MULTIPLE_LAYER_RENDER_TARGETS_CUBEMAP
-	uint CubeMapFace			= IN.RenderTargetArrayIndex;
-	const float3 RayOrigin		= PerViewCubeMapConstantBuffer.PerViewFace[CubeMapFace].ViewPosition.xyz;
-	const float3 RayDirection	= normalize(UVDepthToWorldPosition(
-		IN.UV,
-		PerViewCubeMapConstantBuffer.PerViewFace[CubeMapFace].ViewRenderFarPlane,
-		PerViewCubeMapConstantBuffer.PerViewFace[CubeMapFace].ClipToWorld
-	) - RayOrigin);
+	uint CubeMapFace				= IN.RenderTargetArrayIndex;
+	PerViewConstants CurrentView	= PerViewCubeMapConstantBuffer.PerViewFace[CubeMapFace];
 #else // USE_MULTIPLE_LAYER_RENDER_TARGETS == MULTIPLE_LAYER_RENDER_TARGETS_NONE
-	const float3 RayOrigin		= PerViewConstantBuffer.ViewPosition.xyz;
+	PerViewConstants CurrentView	= PerViewConstantBuffer;
+#endif
+	
+	const float3 RayOrigin		= CurrentView.ViewPosition.xyz;
 	const float3 RayDirection	= normalize(UVDepthToWorldPosition(
 		IN.UV,
-		PerViewConstantBuffer.ViewRenderFarPlane,
-		PerViewConstantBuffer.ClipToWorld
+		CurrentView.ViewRenderFarPlane,
+		CurrentView.ClipToWorld
 	) - RayOrigin);
-#endif
+	
 	return RayDirection;
 }
 
 #include "Volumetrics/volumetrics.type.hlsl"
 ParticipatingMediaDescription SampleParticipatingMedia(float3 PositionWS)
 {
+	float DistanceFromEarthCenter	= length(PositionWS);
+	float HeightDelta				= max(DistanceFromEarthCenter - EARTH_RADIUS_KM, 0.0f);
+	
 	ParticipatingMediaDescription ParticipatingMedia = (ParticipatingMediaDescription)0;
 	
-	ParticipatingMedia.Scattering[SCATTERING_TYPE_MIE]		= 0.01f;
-	ParticipatingMedia.Scattering[SCATTERING_TYPE_RAYLEIGH]	= 0.01f;
+	ParticipatingMedia.Scattering[SCATTERING_TYPE_MIE]		= EarthMieScattering * exp(-HeightDelta * (1.0f / 1.2f));
+	ParticipatingMedia.Scattering[SCATTERING_TYPE_RAYLEIGH]	= EarthRayleighScattering * exp(-HeightDelta * (1.0f / 8.0f));
 	ParticipatingMedia.Extinction = 0.01f;
 	
 	return ParticipatingMedia;
@@ -64,42 +79,24 @@ PSOut PS( PSIn IN )
 {
 	PSOut OUT = (PSOut)0.0f;
 	
-//#if USE_MULTIPLE_LAYER_RENDER_TARGETS == MULTIPLE_LAYER_RENDER_TARGETS_CUBEMAP
-//	switch (IN.RenderTargetArrayIndex)
-//	{
-//	case 0:
-//		OUT.OutLuminance = float4(1, 0, 0, 1);
-//		break;
-//	case 1:
-//		OUT.OutLuminance = float4(0, 1, 0, 1);
-//		break;
-//	case 2:
-//		OUT.OutLuminance = float4(0, 0, 1, 1);
-//		break;
-//	case 3:
-//		OUT.OutLuminance = float4(1, 0, 1, 1);
-//		break;
-//	case 4:
-//		OUT.OutLuminance = float4(1, 1, 0, 1);
-//		break;
-//	case 5:
-//		OUT.OutLuminance = float4(0, 1, 1, 1);
-//		break;
-//	}
-//#endif
-	
+	const float3 RayOriginKm	= GetRayOrigin(IN) * M_TO_KM;
 	const float3 RayDirection	= GetRayDirection(IN);
 	const float3 LightDirection	= AtmosphereConstantBuffer.AtmosphereDirectionalLightDirection;
 	const float LdotV			= dot(LightDirection, RayDirection);
 	
+	float2 AtmosphereIntersection;
+	SphereDescription AtmosphereSphere = InitializeSphereDescription(EARTH_RADIUS_KM_SQUARED);
+	RaySphereIntersection(RayOriginKm + float3(0.0f, 0.0f, EARTH_RADIUS_KM), RayDirection, AtmosphereSphere, AtmosphereIntersection);
+	const float AtmosphereOuterDistance = max(AtmosphereIntersection.x, AtmosphereIntersection.y);
+	
 	IntegrateScatteringParameters Parameters;
 	Parameters.IntegrateDiffusion			= InitializeDiffusionDescription(AtmosphereConstantBuffer.AtmospherePhaseG[0].Value, LdotV);
-	Parameters.IntegratePositionWS			= 0.0f;
+	Parameters.IntegratePositionWS			= RayOriginKm + RayDirection * AtmosphereOuterDistance;
 	Parameters.IntegrateDirection			= RayDirection;
 	Parameters.IntegrateStepCount			= 16;
 	Parameters.IntegrateStepCountRcp		= rcp(Parameters.IntegrateStepCount);
-	Parameters.IntegrateRangeMin			= 0.0f;
-	Parameters.IntegrateRangeMax			= 1.0f;
+	Parameters.IntegrateRangeMin			= EARTH_RADIUS_KM;
+	Parameters.IntegrateRangeMax			= ATMOSPHERE_RADIUS_KM;
 	Parameters.IntegrateLightIlluminance	= AtmosphereConstantBuffer.AtmosphereDirectionalLightIlluminance;
 	
 	IntegrateScatteringResult Result = IntegrateScattering(Parameters);
