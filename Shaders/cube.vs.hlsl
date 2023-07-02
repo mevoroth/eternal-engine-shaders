aa58c9e004c8ee3a5df21467088aee7b4b81e0b5
Daniel Quach <darkskana@gmail.com>
2023-07-02 15:57:10 +0200

    Fixed cube shader

--- Shaders/cube.vs.hlsl
+++ Shaders/cube.vs.hlsl
@@ -4,34 +4,34 @@
 REGISTER_B_PER_VIEW_CONSTANT_BUFFER(														0, 0);
 REGISTER_T(StructuredBuffer<CubePerInstanceInformation> CubePerInstanceStructuredBuffer,	0, 0);
 
-struct PSIn
+struct VSIn
 {
 	uint VertexIndex	: SV_VertexID;
 	uint InstanceIndex	: SV_InstanceID;
 };
 
-float3 CubeVertices[] =
+static float3 CubeVertices[] =
 {
 	float3(-1.0f, -1.0f, -1.0f),
 	float3( 1.0f, -1.0f, -1.0f),
+	float3( 1.0f, -1.0f,  1.0f),
+	float3(-1.0f, -1.0f,  1.0f),
 	float3(-1.0f,  1.0f, -1.0f),
 	float3( 1.0f,  1.0f, -1.0f),
-	float3(-1.0f, -1.0f,  1.0f),
-	float3( 1.0f, -1.0f,  1.0f),
-	float3(-1.0f,  1.0f,  1.0f),
-	float3( 1.0f,  1.0f,  1.0f)
+	float3( 1.0f,  1.0f,  1.0f),
+	float3(-1.0f,  1.0f,  1.0f)
 };
 
-uint Lines[] =
+static uint Lines[] =
 {
 	0, 1,
 	1, 2,
 	2, 3,
-	3, 0
+	3, 0,
 	4, 5,
 	5, 6,
 	6, 7,
-	7, 4
+	7, 4,
 	0, 4,
 	1, 5,
 	2, 6,
