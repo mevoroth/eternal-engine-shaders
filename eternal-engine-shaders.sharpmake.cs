f9ff8a4ec7bdb020a9f3dd65da19b3a3dc447d03
Daniel Quach <daniel@quach.info>
2023-11-04 16:13:46 +0000

    Shaders Sharpmake

---
+++ eternal-engine-shaders.sharpmake.cs
@@ -0,0 +1,25 @@
+﻿using Sharpmake;
+
+[module: Sharpmake.Include(@"..\eternal-engine\eternal-engine.sharpmake.cs")]
+
+namespace EternalEngine
+{
+	[Sharpmake.Generate]
+	public class EternalEngineShadersProject : EternalEngineProject
+	{
+		public EternalEngineShadersProject()
+			: base("shaders")
+		{
+			SourceFilesExtensions.Add(".hlsl");
+			ExtensionBuildTools[".hlsl"] = "FxCompile";
+		}
+
+		public override void ConfigureAll(Configuration InConfiguration, Target InTarget)
+		{
+			base.ConfigureAll(InConfiguration, InTarget);
+
+			InConfiguration.Output = Configuration.OutputType.Utility;
+			InConfiguration.SourceFilesBuildExcludeRegex.Add(@".*\.hlsl$");
+		}
+	}
+}
