e7e03df7cc01f0b8c7e82c27674fca1f563edf1b
Daniel Quach <daniel@quach.info>
2023-12-29 19:26:45 +0100

    Update eternal-engine-shaders.sharpmake.cs

--- eternal-engine-shaders.sharpmake.cs
+++ eternal-engine-shaders.sharpmake.cs
@@ -1,11 +1,11 @@
 ﻿using Sharpmake;
 
-[module: Sharpmake.Include(@"..\eternal-engine\eternal-engine.sharpmake.cs")]
+[module: Sharpmake.Include(@"..\eternal-engine\eternal-engine-project.sharpmake.cs")]
 
 namespace EternalEngine
 {
 	[Sharpmake.Generate]
-	public class EternalEngineShadersProject : EternalEngineProject
+	public class EternalEngineShadersProject : EternalEngineBaseProject
 	{
 		public EternalEngineShadersProject()
 			: base("shaders")
