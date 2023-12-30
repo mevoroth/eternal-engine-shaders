642827fc0c828deffc5c5e789241f3dcb250e768
Daniel Quach <daniel@quach.info>
2023-12-30 16:15:32 +0000

    Create distantobject.pixel.hlsl

---
+++ Shaders/distantobject.pixel.hlsl
@@ -0,0 +1,14 @@
+#include "object.common.hlsl"
+
+REGISTER_T(TextureCube<float3>						SkyTexture,					0, 0);
+REGISTER_S(SamplerState								BilinearSampler,			0, 0);
+
+ShaderPixelOut ShaderPixel( ShaderPixelIn IN )
+{
+	ShaderPixelOut OUT = (ShaderPixelOut)0;
+
+	float3 Normals					= IN.Normal * 2.0f - 1.0f;
+	OUT.Emissive					= SkyTexture.Sample(BilinearSampler, Normals);
+
+	return OUT;
+}
