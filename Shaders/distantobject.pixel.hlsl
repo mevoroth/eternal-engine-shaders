f11f902800ea46a2144fd7eb28c246d7ea826d97
Daniel Quach <daniel@quach.info>
2024-11-11 19:39:36 +0000

    Update distantobject.pixel.hlsl

--- Shaders/distantobject.pixel.hlsl
+++ Shaders/distantobject.pixel.hlsl
@@ -7,8 +7,8 @@ ShaderPixelOut ShaderPixel( ShaderPixelIn IN )
 {
 	ShaderPixelOut OUT = (ShaderPixelOut)0;
 
-	float3 Normals					= IN.Normal * 2.0f - 1.0f;
-	OUT.Emissive					= SkyTexture.Sample(BilinearSampler, Normals);
+	float3 Normals	= IN.Normal * 2.0f - 1.0f;
+	OUT.Emissive	= SkyTexture.Sample(BilinearSampler, Normals);
 
 	return OUT;
 }
