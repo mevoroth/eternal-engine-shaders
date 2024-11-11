da78a19d7bee4a9c2390e5c286753eba1851468b
Daniel Quach <daniel@quach.info>
2024-11-11 19:40:23 +0000

    Private macro

--- Shaders/platform.common.hlsl
+++ Shaders/platform.common.hlsl
@@ -1,8 +1,15 @@
 #ifndef _PLATFORM_COMMON_HLSL_
 #define _PLATFORM_COMMON_HLSL_
 
+#ifndef ETERNAL_USE_PRIVATE
+#define ETERNAL_USE_PRIVATE		(0)
+#endif
+
 #include "vulkan.common.hlsl"
 #include "dx12.common.hlsl"
+
+#if ETERNAL_USE_PRIVATE
 #include "platform.private.common.hlsl"
+#endif
 
 #endif
