6f40046f73d743661dfe7a011d9279f748df5c0f
Daniel Quach <daniel@quach.info>
2026-01-06 20:00:26 +0000

    Vulkan registers

--- Shaders/vulkan.common.hlsl
+++ Shaders/vulkan.common.hlsl
@@ -5,10 +5,10 @@
 #define VULKAN_STRINGIFY(TEXT)														#TEXT
 #define VULKAN_CONCATENATE_REGISTER(Binding, ShaderResource)						Binding ShaderResource
 
-#define REGISTER_T(ShaderResource, Index, Set)										VULKAN_CONCATENATE_REGISTER([[vk::binding(Index /*+ REGISTER_OFFSET_SHADER_RESOURCES*/, Set)]],		ShaderResource)
-#define REGISTER_B(ShaderResource, Index, Set)										VULKAN_CONCATENATE_REGISTER([[vk::binding(Index /*+ REGISTER_OFFSET_CONSTANT_BUFFERS*/, Set)]],		ShaderResource)
+#define REGISTER_T(ShaderResource, Index, Set)										VULKAN_CONCATENATE_REGISTER([[vk::binding(Index + REGISTER_OFFSET_SHADER_RESOURCES, Set)]],		ShaderResource)
+#define REGISTER_B(ShaderResource, Index, Set)										VULKAN_CONCATENATE_REGISTER([[vk::binding(Index + REGISTER_OFFSET_CONSTANT_BUFFERS, Set)]],		ShaderResource)
 #define REGISTER_U(ShaderResource, Index, Set)										VULKAN_CONCATENATE_REGISTER([[vk::binding(Index + REGISTER_OFFSET_UNORDERED_ACCESSES, Set)]],		ShaderResource)
-#define REGISTER_S(ShaderResource, Index, Set)										VULKAN_CONCATENATE_REGISTER([[vk::binding(Index /*+ REGISTER_OFFSET_SAMPLERS*/, Set)]],				ShaderResource)
+#define REGISTER_S(ShaderResource, Index, Set)										VULKAN_CONCATENATE_REGISTER([[vk::binding(Index + REGISTER_OFFSET_SAMPLERS, Set)]],				ShaderResource)
 
 #define CONSTANT_BUFFER(StructName, BufferName, Index, Set)							struct StructName##_Internal { \
 																						StructName InternalValue; \
